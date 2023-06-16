cc550718870969478dacfa97ff433e1869ce6901
dev.yangkong@gmail.com <dev.yangkong@gmail.com>
2023-06-16 18:02:57 +0900

    [ADD] coding-19

---
+++ js/coding-19/coding-19.js
@@ -0,0 +1,10 @@
+// DON'T TOUCH THIS LINE! (please)
+const word = "skateboard"; //Don't change this line!
+
+// YOUR CODE BELOW THIS LINE:
+
+word.slice(-5);
+
+word.slice(-5).replace('o', 'e')
+
+let facialHair = word.slice(-5).replace('o', 'e')
