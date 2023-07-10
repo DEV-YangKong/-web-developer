a52c4dc28a872c95d5211f9883088d799ca80ac2
dev.yangkong@gmail.com <dev.yangkong@gmail.com>
2023-07-10 15:22:58 +0900

    Update p_19.js

--- Javascript/19_MoreStringMethodsPractice/p_19.js
+++ Javascript/19_MoreStringMethodsPractice/p_19.js
@@ -2,9 +2,4 @@
 const word = "skateboard"; //Don't change this line!
 
 // YOUR CODE BELOW THIS LINE:
-
-word.slice(-5);
-
-word.slice(-5).replace('o', 'e')
-
-let facialHair = word.slice(-5).replace('o', 'e')
+let facialHair = word.slice(5).replace("o", "e");
