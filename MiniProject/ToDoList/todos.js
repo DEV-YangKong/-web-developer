81d784d92cd9e7ef14db7b3b1c984bc2b8910f29
dev.yangkong@gmail.com <dev.yangkong@gmail.com>
2023-07-24 17:23:21 +0900

    [update] ToDoList

--- MiniProject/ToDoList/todos.js
+++ MiniProject/ToDoList/todos.js
@@ -1,29 +1,23 @@
 let userInput = prompt("What would you like to do?");
 let toDo = [];
 
-while (userInput !== "quit") {
-  if (userInput === "quit") {
-    breaks;
-  } else if (userInput === "new") {
+while (userInput !== "quit" && userInput !== "q") {
+  if (userInput === "new") {
     userInput = prompt("Enter new Todo");
     toDo.push(userInput);
     console.log(`${userInput} added to list`);
-    userInput = prompt("What would you like to do?");
   } else if (userInput === "list") {
     console.log("******************************");
     for (let list = 0; list < toDo.length; list++) {
       console.log(`${list} : ${toDo[list]}.`);
     }
     console.log("******************************");
-    userInput = prompt("What would you like to do?");
   } else if (userInput === "del") {
     userInput = prompt("Enter index of Todo to delete");
     console.log(`Todo[${userInput}] ${toDo[userInput]} removed`);
     toDo.splice(userInput, 1);
-    userInput = prompt("What would you like to do?");
-  } else {
-    userInput = prompt("You choose wrong command. What would you like to do?");
   }
+  userInput = prompt("What would you like to do?");
 }
 
 console.log(`OK. YOU QUIT THE APP`);
