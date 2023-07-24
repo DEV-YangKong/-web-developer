9c90101d19e63504a142761ae5bacc08efaa84be
dev.yangkong@gmail.com <dev.yangkong@gmail.com>
2023-07-24 17:31:06 +0900

    Update todos.js

--- MiniProject/ToDoList/todos.js
+++ MiniProject/ToDoList/todos.js
@@ -1,11 +1,11 @@
 let userInput = prompt("What would you like to do?");
-let toDo = [];
+const toDo = [];
 
 while (userInput !== "quit" && userInput !== "q") {
   if (userInput === "new") {
-    userInput = prompt("Enter new Todo");
-    toDo.push(userInput);
-    console.log(`${userInput} added to list`);
+    const newToDo = prompt("Enter new Todo");
+    toDo.push(newToDo);
+    console.log(`${newToDo} added to list`);
   } else if (userInput === "list") {
     console.log("******************************");
     for (let list = 0; list < toDo.length; list++) {
@@ -13,9 +13,9 @@ while (userInput !== "quit" && userInput !== "q") {
     }
     console.log("******************************");
   } else if (userInput === "del") {
-    userInput = prompt("Enter index of Todo to delete");
-    console.log(`Todo[${userInput}] ${toDo[userInput]} removed`);
-    toDo.splice(userInput, 1);
+    const delIndex = prompt("Enter index of Todo to delete");
+    console.log(`Todo[${delIndex}] ${toDo[delIndex]} removed`);
+    toDo.splice(delIndex, 1);
   }
   userInput = prompt("What would you like to do?");
 }
