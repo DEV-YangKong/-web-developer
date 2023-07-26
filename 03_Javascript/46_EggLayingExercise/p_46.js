e8129d0a23d1e85c876974c81e5ccaa6b3d212ff
dev.yangkong@gmail.com <dev.yangkong@gmail.com>
2023-07-26 16:40:16 +0900

    [ADD] p_46.js

---
+++ 03_Javascript/46_EggLayingExercise/p_46.js
@@ -0,0 +1,39 @@
+// 더하는 방식
+// let hen = {
+//   name: "Helen",
+//   eggCount: 0,
+//   layAnEgg() {
+//     console.log("EGG");
+//     this.eggCount += 1;
+//   },
+// };
+
+// 증가 하는 방식
+// const hen = {
+//     name: "Helen",
+//     eggCount: 0,
+//     layAnEgg() {
+//       console.log("EGG");
+//       this.eggCount++;
+//     },
+//   };
+
+// 기능은 같지만, 바로 function 하면 유데미 인터프리터 에러 : 더하는 방식
+// const hen = {
+//   name: "Helen",
+//   eggCount: 0,
+//   layAnEgg: function () {
+//     this.eggCount += 1;
+//     return "EGG";
+//   },
+// };
+
+// 기능은 같지만, 바로 function 하면 유데미 인터프리터 에러 : 증가하는 방식
+const hen = {
+  name: "Helen",
+  eggCount: 0,
+  layAnEgg: function () {
+    this.eggCount++;
+    return "EGG";
+  },
+};
